dab72fcdb8499af2f8b64a8323dd3c11044c499c
Nastja <anastasia.findeisen@gmx.de>
2025-05-27 20:45:29 +0200

    Zwischenstand 27.05
    ✅ Führerschein - Page zu Bobo - Page geändert
    ✅ Bobo - Page Inhalt und Formatierung
    ✅ Berufliche Erfahrungen und akademische Ausbildung als Timeline gestaltet

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
